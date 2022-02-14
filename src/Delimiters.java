6bf4b9c1a2bf7b4b1e8c8c10164273d1b132873b
BT_1E10_19 <dennism27@nycstudents.net>
2022-02-14 13:55:25 -0500

    solutions

--- src/Delimiters.java
+++ src/Delimiters.java
@@ -19,7 +19,16 @@ public class Delimiters
     /** Returns an ArrayList of delimiters from the array tokens, as described in part (a). */
     public ArrayList<String> getDelimitersList(String[] tokens)
     {
-        /* to be implemented in part (a) */
+        ArrayList<String> delimiters = new ArrayList<>();
+        for (int i = 0; i < tokens.length; i++)
+        {
+            if (tokens[i].equals(openDel) || tokens[i].equals(closeDel))
+            {
+                delimiters.add(tokens[i]);
+            }
+
+        }
+        return delimiters;
     }
 
     /** Returns true if the delimiters are balanced and false otherwise, as described in part (b).
@@ -27,6 +36,24 @@ public class Delimiters
      */
     public boolean isBalanced(ArrayList<String> delimiters)
     {
-        /* to be implemented in part (b) */
+        int counterOpen = 0;
+        int counterClosed = 0;
+        for (int i = 0; i < delimiters.size(); i++)
+        {
+            if (delimiters.get(i).equals(openDel))
+            {
+                counterOpen++;
+            }
+            else if (delimiters.get(i).equals(closeDel))
+            {
+                counterClosed++;
+            }
+            if(counterClosed > counterOpen)
+            {
+                return false;
+            }
+
+        }
+        return counterOpen >= counterClosed;
     }
 }
